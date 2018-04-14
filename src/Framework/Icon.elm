5ad1e0d12e7e2631806b44269d65989b6bdcd428
Jason Oh <puruzio@gmail.com>
2018-04-14 09:40:12 -0700

    additional icons

--- src/Framework/Icon.elm
+++ src/Framework/Icon.elm
@@ -54,7 +54,7 @@ introspection =
             , ( icon MobileNotification 32, "icon MobileNotification 32" )
             , ( icon MobileNotification2 32, "icon MobileNotification2 32" )
             , ( icon ChevronDown 32, "icon ChevronDown 32" )
-            , ( icon Ic_search_black 32, "icon Ic_search_black 32" )
+            , ( icon Ic_search_black 100, "icon Ic_search_black 100" )
             ]
           )
         ]
@@ -114,6 +114,15 @@ icon logo size =
             Ic_search_black ->
                 ic_search_black size
 
+            Ic_setting_black ->
+                ic_setting_black size
+
+            Ic_save_black ->
+                ic_save_black size
+
+            Ic_refresh_black ->
+                ic_refresh_black size
+
 
 {-| Type of logos
 -}
@@ -130,6 +139,9 @@ type Icon
     | MobileNotification2
     | ChevronDown
     | Ic_search_black
+    | Ic_setting_black
+    | Ic_save_black
+    | Ic_refresh_black
 
 
 home : Int -> Html.Html msg
@@ -241,16 +253,45 @@ chevronDown size color =
         ]
 
 
+
+--Html.Attributes.style [ ( "height", toString 50 ++ "px" ) ],
+
+
 ic_search_black : Int -> Html msg
 ic_search_black size =
-    Svg.svg [ Html.Attributes.style [ ( "height", toString size ++ "px" ) ], SA.viewBox "0 0 32 32", SA.fill "#000000" ]
+    Svg.svg [ SA.height <| toString size, SA.viewBox "0 0 24 24", SA.fill "#000000" ]
         [ Svg.path
             [ SA.d "M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"
             ]
             []
-        , Svg.path
-            [ SA.fill "none"
-            , SA.d "M0 0h24v24H0z"
+        ]
+
+
+ic_setting_black : Int -> Html msg
+ic_setting_black size =
+    Svg.svg [ SA.height <| toString size, SA.viewBox "0 0 24 24", SA.fill "#000000" ]
+        [ Svg.path
+            [ SA.d "M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z"
+            ]
+            []
+        ]
+
+
+ic_save_black : Int -> Html msg
+ic_save_black size =
+    Svg.svg [ SA.height <| toString size, SA.viewBox "0 0 24 24", SA.fill "#000000" ]
+        [ Svg.path
+            [ SA.d "M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z"
+            ]
+            []
+        ]
+
+
+ic_refresh_black : Int -> Html msg
+ic_refresh_black size =
+    Svg.svg [ SA.height <| toString size, SA.viewBox "0 0 24 24", SA.fill "#000000" ]
+        [ Svg.path
+            [ SA.d "M17.65 6.35C16.2 4.9 14.21 4 12 4c-4.42 0-7.99 3.58-7.99 8s3.57 8 7.99 8c3.73 0 6.84-2.55 7.73-6h-2.08c-.82 2.33-3.04 4-5.65 4-3.31 0-6-2.69-6-6s2.69-6 6-6c1.66 0 3.14.69 4.22 1.78L13 11h7V4l-2.35 2.35z"
             ]
             []
         ]
